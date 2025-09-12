70ad61e2c75dfc596e4dff8952923d408ccc7d77
Nahal Abdulla Puthiyeduth <nahalabdulla2@gmail.com>
2025-09-12 22:26:45 +0000

    Finsihed the development of this simple game
    ADDED the check fucntion to see who won for the round
    ADDED the playgame function to start the game for 5 rounds and see who won the game
    ADDED a way to initialize the game.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){ //A function to get Rock, Paper or Scissors using Random
     rand = (Math.random() * 100) //Generates a Random number between 1 to 99
-    console.log(rand)
     if(rand < 33) //If Rand is less than 33, Returns Rock
-        return "r"
+        return 'r'
     else if (rand < 66) //If Rand is greater than 33 But Less than 66, Returns Paper
-        return "p"
+        return 'p'
     else //If above statements ain't true, it will return us Scissors
-        return "s"
+        return 's'
 } //33% chance to get either Rock Paper or Scissors
 
-function getHumanChoice(){
+function getHumanChoice(){ //A dialog box appears and expects an answer of 'r', 'p' or 's'. Continues to ask until it gets the proper input
     let breakFlag = true;
     while (breakFlag == true) {
         let choice = prompt("What will you pick? (r - Rock, p - Paper or s - Scissor)");
@@ -24,4 +26,40 @@ function getHumanChoice(){
     }
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice, compChoice){ //Function to check who won? It first check if its a Tie, if true, both gets a point. Then checks if the human player beats the computer, if true, human get a point. If all condition fails, the computer gets a point
+    console.log(`You Chose '${humanChoice}' and the Computer Chose '${compChoice}'`)
+
+    if(humanChoice == compChoice){
+        console.log("Its a Tie!!! Both gets points")
+        humanScore++
+        computerScore++
+    }
+    else if ((humanChoice == 'r' && compChoice == 's') || (humanChoice == 's' && compChoice == 'p') || (humanChoice == 'p' && compChoice == 'r')){
+        console.log(`You won this round, as ${humanChoice} beats ${compChoice}`)
+        humanScore++
+    }
+    else{
+        console.log(`You lost this round, as ${compChoice} beats ${humanChoice}`)
+        computerScore++
+    }
+
+    console.log(`Current Scores: You: ${humanScore} || Computer: ${computerScore}`)
+}
+
+function playGame(){ //starts the game for 5 rounds (using for loop) and checks the score to know if the human won or the computer won
+    alert("LETS PLAY ROCK, PAPER and SCISSORS!!!!!!!")
+    for(let i = 0; i < 5; i++)
+    {   
+        console.log(`ROUND ${i + 1}`)
+        playRound(getHumanChoice(), getComputerChoice())
+    }
+
+    if(humanScore == computerScore)
+        alert(`Its a tie game with ${humanScore} to ${computerScore}`)
+    else if (humanScore > computerScore)
+        alert(`You won the game with ${humanScore} to ${computerScore}`)
+    else
+        alert(`Too bad. You lost the game with ${humanScore} to ${computerScore}`)
+}
+
+playGame() //starts the game
