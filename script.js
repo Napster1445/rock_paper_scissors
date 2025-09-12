b07b72890711a2676aa14627ca71ef443418ed3f
Nahal Abdulla Puthiyeduth <nahalabdulla2@gmail.com>
2025-09-12 21:42:53 +0000

    Made the getComputerChoise()
    That will output Rock, Paper or Scissors based on a Random number that was selected

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!!!")
+function getComputerChoice(){ //A function to get Rock, Paper or Scissors using Random
+    rand = (Math.random() * 100) //Generates a Random number between 1 to 99
+
+    if(rand < 33) //If Rand is less than 33, Returns Rock
+        return "Rock"
+    else if (rand < 66) //If Rand is greater than 33 But Less than 66, Returns Paper
+        return "Paper"
+    else //If above statements ain't true, it will return us Scissors
+        return "Scissors"
+} //33% chance to get either Rock Paper or Scissors
+
+console.log(getComputerChoice())
